1987327aae77a5df8aafef2a03c15de9328e6cb1
Arseny Radushin <radushin.arseny@yandex.ru>
2018-06-20 05:35:17 +0300

    second

--- run.py
+++ run.py
@@ -142,7 +142,7 @@ def handle_message(message):
 def callback_inline(call):
     if call.message:
         if call.data=='yes':
-            for data in call.message.entities:
+            for data in call.message.entities.mention:
                 print((data))
 
 
