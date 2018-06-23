2e63fb8efe300229275690a1c8645aca396b7e7f
Arseny Radushin <radushin.arseny@yandex.ru>
2018-06-23 15:46:14 +0300

    logger added

--- run.py
+++ run.py
@@ -1,10 +1,14 @@
 import telebot
 import config
+import time
+import logging
 import json
 from db_tool import DbQuery
 
 db_query = DbQuery()
 bot = telebot.TeleBot(config.token)
+logging.basicConfig(filename="sample.log", level=logging.INFO)
+
 
 def stage_check(message):
     query = """SELECT stage
@@ -192,5 +196,6 @@ while True:
     # bot.polling(none_stop=True)
     try:
         bot.polling(none_stop=True)
-    except:
-        continue
+    except Exception as e:
+        logging.error(e)
+        time.sleep(15)
